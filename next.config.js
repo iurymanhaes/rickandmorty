fb35388f8fcd3c06430e04f1323a423fa387f20b
iurymanhaes <56461818+iurymanhaes@users.noreply.github.com>
2023-04-02 21:58:07 -0300

    projeto rick and morty

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+    fontLoaders: [
+      { loader: "@next/font/google", options: { subsets: ["latin"] } },
+    ],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rickandmortyapi.com',
+        port: '',
+        pathname: '/api/character/avatar/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
